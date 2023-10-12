d5c507c9266c60c1e8546ff50c483503bad6dd61
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-10-12 18:46:51 +0200

    Local minimal functionality added

--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -1,7 +1,23 @@
+<script lang="ts">
+	const word = {
+		text: 'Auto',
+		article: 'das',
+		choices: ['der', 'die', 'das']
+	};
+
+	function submitChoice(choice: string): void {
+		if (choice === word.article) {
+			alert('Excellent!');
+		} else {
+			alert('Try again!');
+		}
+	}
+</script>
+
 <main>
 	<h1>Play the game here</h1>
-	<h3>Sustantiv</h3>
-	<button>der</button>
-	<button>die</button>
-	<button>das</button>
+	<h3>{word.text}</h3>
+	{#each word.choices as choice}
+		<button on:click={() => submitChoice(choice)}>{choice}</button>
+	{/each}
 </main>
