1ff078a2a328c5c5c5383e377e9b0e50bf5e34c5
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-10-26 20:10:39 +0200

    Remove immediate feedback during practice

--- src/routes/practice/Practice.svelte
+++ src/routes/practice/Practice.svelte
@@ -1,47 +1,33 @@
 <script lang="ts">
 	import ArticleButton from '$lib/components/ArticleButton.svelte';
 	import type { Answer } from '$lib/types/answers.js';
-	import type { Word } from '$lib/types/words';
+	import type { Articles, Word } from '$lib/types/words';
 
 	export let words: Word[];
 
 	let index = 0;
-	let score = 0;
-	let isValidPlay = true;
 	let answers: Answer[] = [];
 
-	function submitChoice(choice: string): void {
-		if (choice === words[index].article) {
-			if (isValidPlay) {
-				score += 1;
-			} else {
-				isValidPlay = true;
-			}
-			index += 1;
-		} else {
-			alert('Try again!');
-			isValidPlay = false;
-		}
+	function submitChoice(word: Word, choice: Articles): void {
+		answers.push({ word, choice });
+		index += 1;
 	}
 
 	function resetGame(): void {
 		index = 0;
-		score = 0;
-		isValidPlay = true;
 	}
 </script>
 
 <h1>Play the game here</h1>
 {#if index < words.length}
-	<h3>{words[index].text}</h3>
+	<h2>{words[index].text}</h2>
 	<div class="choices__container">
 		{#each words[index].choices as choice}
-			<ArticleButton onClickHandler={() => submitChoice(choice)} {choice} />
+			<ArticleButton onClickHandler={() => submitChoice(words[index], choice)} {choice} />
 		{/each}
 	</div>
-	<h1>{score}</h1>
 {:else}
-	<h2>Finished! Your score: {score}</h2>
+	<h2>Finished! Show Summary PLZ</h2>
 	<button on:click={resetGame}>Play again!</button>
 {/if}
 
