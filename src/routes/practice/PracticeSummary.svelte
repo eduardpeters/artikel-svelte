f79dae113b959ce506490f6a5c439807cd4f8a23
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-10-26 20:50:36 +0200

    Add Summary Table

--- src/routes/practice/PracticeSummary.svelte
+++ src/routes/practice/PracticeSummary.svelte
@@ -14,4 +14,49 @@
 </script>
 
 <h2>Finished! Your score: {score}</h2>
+<div class="summary__table">
+	<div class="summary__table-row summary__table-header">
+		<div>Noun</div>
+		<div>Article</div>
+		<div>Your answer</div>
+		<div>Score</div>
+	</div>
+	{#each answers as answer}
+		<div class="summary__table-row">
+			<div>{answer.word.text}</div>
+			<div>{answer.word.article}</div>
+			<div>
+				{answer.choice}
+			</div>
+			<div>{answer.word.article === answer.choice ? 'YAY' : 'NAY'}</div>
+		</div>
+	{/each}
+</div>
 <button on:click={resetGame}>Play again!</button>
+
+<style>
+	.summary__table {
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+		width: 80%;
+	}
+
+	.summary__table-row {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+		align-items: center;
+		width: 100%;
+	}
+
+	.summary__table-row > div {
+		text-align: center;
+		width: 25%;
+	}
+
+	.summary__table-header {
+		font-weight: bold;
+	}
+</style>
