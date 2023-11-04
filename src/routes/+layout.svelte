dc814f26e17af75d9eb923bc3c73d5cfe55fe18c
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-11-04 08:24:39 +0100

    Update page title

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,4 @@
+<svelte:head>
+	<title>DerDieDas</title>
+</svelte:head>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,5 @@
 	<h1><em>Willkommen</em> to DerDieDas</h1>
 	<p>A game to practice basic german skills</p>
 	<a href="/practice">Try it out</a>
+	<a href="/blog">Development Short Blogs</a>
 </main>
