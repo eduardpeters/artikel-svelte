f489d7f9782c2f3a0f9292a753cab1861929742f
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-11-05 17:48:04 +0100

    Style sections

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,25 +9,31 @@
 			<a href="/blog">Microblogs</a>
 		</nav>
 	</div>
-	<h2>The challenge</h2>
-	<p>
-		Play through your choice of some number of german nouns for which you will have to correctly
-		select the article. Your score will be the length your streak selecting the correct articles
-	</p>
-	<p>
-		An arcade inspired leaderboard is available to measure up to the competition. Good luck getting
-		your mark to the top!
-	</p>
-	<h2>Practice for a while</h2>
-	<p>
-		In practice mode, you'll receive a random set of 10 nouns and play through them all. At the end,
-		you'll be able to see how well you did and get feedback on any mistakes.
-	</p>
-	<h3>Microblogs</h3>
-	<p>
-		Getting from idea to launch is not an easy journey. In order to blow off some steam, these small
-		fragments of the story of this project have kept development open and reflective.
-	</p>
+	<section>
+		<h2>The challenge</h2>
+		<p>
+			Play through your choice of some number of german nouns for which you will have to correctly
+			select the article. Your score will be the length your streak selecting the correct articles
+		</p>
+		<p>
+			An arcade inspired leaderboard is available to measure up to the competition. Good luck
+			getting your mark to the top!
+		</p>
+	</section>
+	<section>
+		<h2>Practice for a while</h2>
+		<p>
+			In practice mode, you'll receive a random set of 10 nouns and play through them all. At the
+			end, you'll be able to see how well you did and get feedback on any mistakes.
+		</p>
+	</section>
+	<section>
+		<h2>Microblogs</h2>
+		<p>
+			Getting from idea to launch is not an easy journey. In order to blow off some steam, these
+			small fragments of the story of this project have kept development open and reflective.
+		</p>
+	</section>
 </main>
 
 <style>
@@ -63,5 +69,12 @@
 
 	nav > a {
 		color: #e76f51;
+	}
+
+	section {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+		padding: 1rem;
 	}
 </style>
