03455e693519acdc27f9936e50be5c62b66791f8
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-11-05 18:34:22 +0100

    Add diagonal icon to NavAnchor component

--- src/lib/components/NavAnchor.svelte
+++ src/lib/components/NavAnchor.svelte
@@ -1,20 +1,28 @@
 <script lang="ts">
+	import arrowIcon from '$lib/icons/diagonal-arrow-icon.svg';
+
 	export let route: string;
 	export let title: string;
 </script>
 
-<a href={route}>{title}</a>
+<a href={route}>
+	{title}
+	<img src={arrowIcon} alt="A top right facing diagonally directed arrow" height={25} width={25} />
+</a>
 
 <style>
 	a {
 		all: unset;
+		align-items: center;
 		background-color: #e76f51;
-		color: black;
-		padding: 0.5rem 1rem;
 		border-radius: 1rem;
-		width: fit-content;
-		font-weight: bold;
+		color: black;
 		cursor: pointer;
+		display: flex;
+		font-weight: bold;
+		gap: 0.2rem;
+		padding: 0.5rem 1rem;
+		width: fit-content;
 	}
 
 	a:hover {
