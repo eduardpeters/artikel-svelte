7bfe42f9d9ad935a03522b9eb319c2c5cdae85d1
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-11-05 17:30:52 +0100

    Add content to landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,29 @@
 <main>
 	<h1><em>Willkommen</em> to DerDieDas</h1>
 	<p>A game to practice basic german skills</p>
-	<a href="/practice">Try it out</a>
-	<a href="/blog">Development Short Blogs</a>
+	<h3>Here are the shortcuts:</h3>
+	<nav>
+		<a href="/">Challenge</a>
+		<a href="/practice">Practice</a>
+		<a href="/blog">Microblogs</a>
+	</nav>
+	<h2>The challenge</h2>
+	<p>
+		Play through your choice of some number of german nouns for which you will have to correctly
+		select the article. Your score will be the length your streak selecting the correct articles
+	</p>
+	<p>
+		An arcade inspired leaderboard is available to measure up to the competition. Good luck getting
+		your mark to the top!
+	</p>
+	<h2>Practice for a while</h2>
+	<p>
+		In practice mode, you'll receive a random set of 10 nouns and play through them all. At the end,
+		you'll be able to see how well you did and get feedback on any mistakes.
+	</p>
+	<h3>Microblogs</h3>
+	<p>
+		Getting from idea to launch is not an easy journey. In order to blow off some steam, these small
+		fragments of the story of this project have kept development open and reflective.
+	</p>
 </main>
