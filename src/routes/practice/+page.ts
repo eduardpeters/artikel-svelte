00e4c56fac2fc47409f44418950407edf49c7a3a
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-11-27 20:05:30 +0100

    Update to use SvelteKit fetch

--- src/routes/practice/+page.ts
+++ src/routes/practice/+page.ts
@@ -1,7 +1,7 @@
 import { PUBLIC_API_BASE_URL } from '$env/static/public';
 import type { Word } from '$lib/types/words';
 
-export async function load(): Promise<{ words: Word[] }> {
+export async function load({ fetch }): Promise<{ words: Word[] }> {
 	const requestUrl = `${PUBLIC_API_BASE_URL}questions`;
 	const res = await fetch(requestUrl);
 	const words = await res.json();
