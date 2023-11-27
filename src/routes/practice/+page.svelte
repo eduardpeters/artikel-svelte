e37aeab83f3fe806297ce130eee10e56aefb299a
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-11-27 20:10:51 +0100

    Add back anchor to practice page

--- src/routes/practice/+page.svelte
+++ src/routes/practice/+page.svelte
@@ -7,12 +7,18 @@
 
 <main>
 	<Practice {words} />
+	<a href="/">Return to Home</a>
 </main>
 
 <style>
 	main {
+		align-items: center;
 		display: flex;
 		flex-flow: column nowrap;
-		align-items: center;
+		gap: 1rem;
+	}
+
+	a {
+		color: #e76f51;
 	}
 </style>
