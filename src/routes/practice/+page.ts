e2cd817b89e1cb8e81b0ca51fde5fd05a3b6c341
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2023-11-27 19:59:56 +0100

    Add practice route loader

---
+++ src/routes/practice/+page.ts
@@ -0,0 +1,9 @@
+import { PUBLIC_API_BASE_URL } from '$env/static/public';
+import type { Word } from '$lib/types/words';
+
+export async function load(): Promise<{ words: Word[] }> {
+	const requestUrl = `${PUBLIC_API_BASE_URL}questions`;
+	const res = await fetch(requestUrl);
+	const words = await res.json();
+	return { words };
+}
