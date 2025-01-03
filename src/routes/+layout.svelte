60d4821c096615e1bcfcf38eacdd434a789b9031
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-01-03 07:40:48 +0100

    Style home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div>
+	{@render children()}
+</div>
+
+<style>
+	div {
+		padding: 1rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,32 @@
+<script lang="ts">
+	import LargeNavLink from '$lib/components/LargeNavLink.svelte';
+</script>
+
 <main>
-	<h1><em>Willkommen</em> to DerDieDas</h1>
+	<h1>
+		<em>Willkommen</em> to <span class="der">Der</span><span class="die">Die</span><span class="das">Das</span>
+	</h1>
 	<p>A game to practice basic german article skills</p>
-	<a href="play">Play!</a>
+	<LargeNavLink to="/play" title="Play!" />
 </main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	.der {
+		color: var(--color-der);
+	}
+
+	.die {
+		color: var(--color-die);
+	}
+
+	.das {
+		color: var(--color-das);
+	}
+</style>
