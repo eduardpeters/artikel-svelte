11c3a0d16d8ba0f3355c95b26b8e5dcc69a4e0f8
Eduard Peters <76105887+eduardpeters@users.noreply.github.com>
2025-01-03 21:47:16 +0100

    Add LoadingIndicator

--- src/lib/components/Play.svelte
+++ src/lib/components/Play.svelte
@@ -3,6 +3,7 @@
 	import { articles } from '$lib/constants/articles';
 	import QuestionsService from '$lib/services/QuestionsService';
 	import type { Question } from '$lib/types/questions';
+	import LoadingIndicator from './LoadingIndicator.svelte';
 
 	let loadingQuestion = $state(true);
 	let loadingFeedback = $state(false);
@@ -45,25 +46,44 @@
 	}
 </script>
 
-<h1>Let's practice</h1>
-<p>What is the article for <em>{question ? question.noun : '...'}</em>?</p>
-{#if loadingQuestion}
-	<span>Loading...</span>
-{:else if !loadingQuestion && question}
-	<div>
-		{#each articles as article}
-			<button disabled={loadingQuestion} onclick={() => answerQuestion(question!.id, article.id)}
-				>{article.article}</button
-			>
-		{/each}
-	</div>
-{/if}
-{#if feedback === null && loadingFeedback}
-	<div>Checking answer...</div>
-{:else if !loadingFeedback && feedback}
-	<div>Result: {feedback}</div>
-	<button onclick={resetQuestion}>Play again</button>
-{/if}
-{#if !loadingQuestion && question == null}
-	<button onclick={resetQuestion}>Reload question</button>
-{/if}
+<main>
+	<h1>Let's practice</h1>
+	<p>What is the article for <em>{question ? question.noun : '...'}</em>?</p>
+	{#if loadingQuestion}
+		<LoadingIndicator />
+	{:else if !loadingQuestion && question}
+		<div>
+			{#each articles as article}
+				<button disabled={loadingFeedback} onclick={() => answerQuestion(question!.id, article.id)}
+					>{article.article}</button
+				>
+			{/each}
+		</div>
+	{/if}
+	{#if feedback === null && loadingFeedback}
+		<div class="loading-feedback__container">
+			<span> Checking answer... </span>
+			<LoadingIndicator />
+		</div>
+	{:else if !loadingFeedback && feedback}
+		<div>Result: {feedback}</div>
+		<button onclick={resetQuestion}>Play again</button>
+	{/if}
+	{#if !loadingQuestion && question == null}
+		<button onclick={resetQuestion}>Reload question</button>
+	{/if}
+</main>
+
+<style>
+	main,
+	.loading-feedback__container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 1rem;
+	}
+
+	p {
+		margin: 0;
+	}
+</style>
